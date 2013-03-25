de7f3bc07547f1a39ccb36861e48fb85a17b391d
star <ilife5@qq.com>
2013-03-25 22:40:12 +0800

    新建domTree
    domTree.js 根据给定的数组，如：['div#rootId.rootCls.rootCls2[rootName]',
    ['div[sonName]#sonId', ['div.son2Cls[son2Name]']],
    ['div']]，可生成文档树，数组可进行递归定义。
    数组的第一个参数为子树的根节点，同一个数组其他的数据为该节点的子节点。
    每一个节点（即dom）可以使用如dom#id.class1.calss2[name]的方式进行约定。

---
+++ mann/domTree.js
@@ -0,0 +1,86 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: jifeng.yao
+ * Date: 13-3-21
+ * Time: 下午5:05
+ * To change this template use File | Settings | File Templates.
+ */
+
+/**
+ * 将分析array中的节点，返回domtree，并将dom的名字绑定在相应的对象上
+ */
+(function () {
+    var localObj;
+
+    /**
+     * @param obj
+     * @param arr
+     * treeBuild(obj, ["div#rootId.rootClass[rootName]", ["div#childId.childClass", ["child#grandsonId.grandsonClass", ...]]])
+     */
+    function build(arr, obj) {
+        if (obj) {
+            localObj = obj;
+        }
+        return treeParse(arr);
+    }
+
+    function treeParse(tree) {
+        var root = domParse(tree[0]), i = 1;
+        if (tree.length > 1) {
+            while (tree[i]) {
+                domAppend(root, treeParse(tree[i]));
+                i++;
+            }
+        }
+        return root;
+    }
+
+    function domParse(string) {
+        var dom,
+            classArr = [];
+        string.replace(/^\w+/,function (match) {
+            //取出开始的节点名字初始化节点
+            dom = document.createElement(match);
+            return '';
+        }).replace(/#(\w+)/,function () {
+                if (arguments.length > 1) {
+                    dom.id = arguments[1];
+                }
+                return '';
+            }).replace(/\.(\w+)/g,function () {
+                //将className暂时存储在classArr中
+                if (arguments.length > 1) {
+                    classArr.push(arguments[1]);
+                }
+                return '';
+            }).replace(/\[(\w+)\]/, function () {
+                if (arguments.length > 1) {
+                    dom.setAttribute('data-name', arguments[1]);
+                    if (localObj) {
+                        localObj[dom.name] = dom;
+                    }
+                }
+                return '';
+            });
+        //如果有className，将其赋值给dom.className
+        if (classArr.length > 0) {
+            dom.className = classArr.join(' ');
+        }
+        return dom;
+    }
+
+    function domAppend(root, child) {
+        return root.appendChild(child);
+    }
+
+    if (typeof define === 'function') {
+        define(function () {
+                return {
+                    buildTree:build
+                };
+            }
+        );
+    } else {
+        window.buildTree = build;
+    }
+})();

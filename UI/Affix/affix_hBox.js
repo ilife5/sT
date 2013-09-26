cda5d1981cb5bb3321a8b9c060211c889ab88f1d
ilife5 <ilife5@qq.com>
2013-09-26 23:49:15 +0800

    add hB0x instance

--- UI/Affix/affix_hBox.js
+++ UI/Affix/affix_hBox.js
@@ -1,42 +1,52 @@
-/**
- * 竖向有边界的浮动效果
- */
+;(function($, exports, Affix, undefined) {
+    /**
+     * 竖向有边界的浮动效果
+     */
 
-var getValue = Affix.getValue;
+    var getValue = Affix.getValue;
 
-/**
- * @param opts
- * @constructor
- */
-function Affix_hBox(opts) {
-    var top = opts.top,
-        left = opts.left,
-        rangeTop = opts.rangeTop,
-        rangeBottom = opts.rangeBottom,
-        check = opts.check || function () {
-            return true;
-        },
-        floatArea = opts.floatArea,
-        $floatArea = $(floatArea),
-        affix;
+    /**
+     * @param opts
+     * @method
+     */
+    function affix_hBox(opts) {
+        var top = opts.top,
+            left = opts.left,
+            rangeTop = opts.rangeTop,
+            rangeBottom = opts.rangeBottom,
+            check = opts.check || function () {
+                return true;
+            },
+            floatArea = opts.floatArea,
+            $floatArea = $(floatArea),
+            affix;
 
-    affix = new Affix({
-        el:$floatArea,
-        heightHack:true,
-        range:[rangeTop, null, function() {
-            return getValue(rangeBottom) + $(window).height() - $floatArea.outerHeight();
-        }],
-        top:top,
-        everyTime:true,
-        left:left,
-        recoveryStyle:{
-            top:function() {
-                if($(document).scrollTop() + $floatArea.outerHeight() >= getValue(rangeBottom) && check()) {
-                    return getValue(rangeBottom) - getValue(rangeTop);
-                } else {
-                    return 0;
+        affix = new Affix({
+            el:$floatArea,
+            heightHack:true,
+            range:[rangeTop, null, function() {
+                return getValue(rangeBottom) + $(window).height() - $floatArea.outerHeight();
+            }],
+            top:top,
+            everyTime:true,
+            left:left,
+            recoveryStyle:{
+                top:function() {
+                    var scrollTop = $(document).scrollTop(),
+                        floatAreaH = $floatArea.outerHeight(),
+                        bottom = getValue(rangeBottom);
+                    if(scrollTop + floatAreaH >= bottom && check()) {
+                        return bottom - getValue(rangeTop) - floatAreaH;
+                    } else {
+                        return 0;
+                    }
                 }
             }
-        }
-    });
-}
+        });
+
+        return affix;
+    }
+
+    exports.affix_hBox = affix_hBox;
+
+})(jQuery, window, window.Affix);
